cd13bf180fb1e8e0bf18791ea69bea8117518fc5
Vyacheslav <v.zelinsky@itransition.com>
2021-11-23 16:01:35 +0200

    Deleted url test

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -9,13 +9,3 @@ it("Test getCountries API", async () => {
 
   expect(getCountriesMock).toBeCalledTimes(1);
 });
-
-it("Test getCountries API fetch", async () => {
-  const getCountriesMock = jest.fn() as jest.MockedFunction<typeof getCountries>;
-
-  getCountriesMock.mockImplementation(getCountries)
-
-  await getCountriesMock();
-
-  expect(getCountriesMock).toBeCalledWith();
-});
